fa78db5c952ffee16c0726646820336e851d4e99
xhu <hxtheone@gmail.com>
2016-06-22 02:03:03 +0800

    update

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ $(() => {
   for (let block of $('.block')) {
     var $block = $(block);
     var blockTopOffset = $block.css('top');
-    var duration = Math.random() * 1800 + 200;
+    var duration = Math.random() * 1000;
     $block.css({
       top: 0
     }).animate({
