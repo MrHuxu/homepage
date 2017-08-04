563de71fc6ac54e38baa0670c58f548b8f25e9b1
xhu <hxtheone@gmail.com>
2017-08-05 01:30:27 +0800

    start to work on d3

---
+++ data.js
@@ -0,0 +1,35 @@
+const websites = [{
+  favicon : 'https://raw.githubusercontent.com/MrHuxu/img-repo/master/blog/favicon.ico',
+  url     : 'blog.xhu.me',
+  label   : 'My blog'
+}, {
+  favicon : 'http://h.xhu.me/favicon.ico',
+  url     : 'h.xhu.me',
+  label   : 'Little CMS'
+}, {
+  favicon : 'https://raw.githubusercontent.com/MrHuxu/MySnippets/master/vendor/icon/logo.ico',
+  url     : 'snippets.xhu.me',
+  label   : 'Snippets manager'
+}];
+
+const skills = [{
+  key   : 'Languages',
+  value : 'Ruby, JavaScript, some C++ & Go, a little Scala',
+}, {
+  key   : 'Frameworks',
+  value : 'RoR, Node.js development stack & Front-end'
+}];
+
+const career = [{
+  timeRange   : '2014/07 ~ Now',
+  description : {
+    url   : 'http://freewheel.tv/',
+    label : 'FreeWheel Beijing Office'
+  }
+}, {
+  timeRange   : '2010/09 ~ 2014/06',
+  description : {
+    url   : 'http://www.hit.edu.cn/',
+    label : 'Harbin Institute of Technology'
+  }
+}];
--- main.js
+++ main.js
@@ -1,9 +1,36 @@
-const career = [
-  {
-    timeRange: '2014/07 ~ Now:',
-    description: {
-      url: 'http://freewheel.tv/',
-      label: 'FreeWheel Beijing Office'
-    }
-  }
-]
+const { select } = d3;
+
+const bodyWidth = document.body.clientWidth;
+const bodyHeight = document.body.clientHeight;
+const svg = select('body')
+  .append('svg')
+  .attr('width', bodyWidth)
+  .attr('height', bodyHeight);
+
+// const containerWidth = 600;
+// const containerHeight = 400;
+// const detailContainer = svg.append('g')
+//   .attr('transform', `translate(${(bodyWidth - containerWidth) / 2}, 200)`);
+
+const websiteEles = svg.selectAll('.website-ele')
+  .data(websites)
+  .enter()
+  .append('text')
+  .attr('class', 'website-ele')
+  .attr('y', (d, i) => (i + 1) * 20)
+  .text(d => d.label)
+  .style('color', '#FFFFFF');
+
+const skillEles = svg.selectAll('.skill-ele')
+  .data(skills)
+  .enter()
+  .append('text')
+  .attr('class', 'skill-ele')
+  .text(d => `${d.key} : ${d.value}`);
+
+const careerEles = svg.selectAll('.career-ele')
+  .data(career)
+  .enter()
+  .append('text')
+  .attr('class', 'career-ele')
+  .text(d => `${d.timeRange} : ${d.description.label}`);
