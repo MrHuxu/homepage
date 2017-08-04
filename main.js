e7159a25a1a35b58333b141e1cac9fa3956f30bb
xhu <hxtheone@gmail.com>
2017-08-04 23:58:07 +0800

    remove old code, ready to use d3

--- main.js
+++ main.js
@@ -1,34 +1,9 @@
-$(() => {
-  $('.detail li').addClass('animated fadeInLeft');
-  $('.detail .label').addClass('animated fadeInRight');
-
-  var bindHoverEvent = (label) => {
-    var $label = $(label);
-    var labelLeftOffset = $label.css('left');
-    var labelWidth = $label.css('width');
-    $label.hover(() => {
-      $label.animate({
-        left: -(parseInt(labelWidth) - 25)
-      }, 300);
-    }, () => {
-      $label.animate({
-        left: labelLeftOffset
-      }, 300);
-    });
-  };
-
-  for (let label of $('.left-panel .label')) {
-    bindHoverEvent(label);
+const career = [
+  {
+    timeRange: '2014/07 ~ Now:',
+    description: {
+      url: 'http://freewheel.tv/',
+      label: 'FreeWheel Beijing Office'
+    }
   }
-  for (let block of $('.block')) {
-    var $block = $(block);
-    var blockTopOffset = $block.css('top');
-    var duration = Math.random() * 1000;
-    $block.css({
-      top: 0
-    }).animate({
-      opacity: 1,
-      top: blockTopOffset
-    }, duration);
-  }
-});
+]
