8842183a0497290378c4b825e0cfd31981b614eb
MrHuxu <hxtheone@gmail.com>
2018-01-27 17:31:31 +0800

    add the config file and update the config code of go server

--- main.go
+++ main.go
@@ -1,16 +1,11 @@
 package main
 
 import (
+	"github.com/MrHuxu/homepage/server/config"
 	"github.com/MrHuxu/homepage/server/web"
 )
 
-const (
-	PORT           = 11011
-	TEMPLATES_PATH = "server/web/templates/*"
-	PUBLIC_PATH    = "server/public"
-)
-
 func main() {
-	server := web.NewServer(PORT, TEMPLATES_PATH, PUBLIC_PATH)
+	server := web.NewServer(config.Config)
 	server.Run()
 }
